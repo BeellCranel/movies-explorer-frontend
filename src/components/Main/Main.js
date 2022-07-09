c9563def25c661c0577b64e2a9ff50ab8bbad18f
Dergach Roman <dergachroma@gmail.com>
2022-07-09 19:51:44 +0300

    feat: сделал секчию ManePageHeader адаптивной

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import "./Main.scss";
-import MainPageNav from "../MainPageNav/MainPageNav";
+import MainPageHeader from "../MainPageHeader/MainPageHeader";
 import About from "../About/About";
 import Technologies from "../Technologies/Technologies";
 import Student from "../Student/Student";
@@ -9,7 +9,7 @@ function Main() {
   return (
     <>
       <main className="content">
-        <MainPageNav />
+        <MainPageHeader />
         <About />
         <Technologies />
         <Student />
---
+++ src/components/MainPageHeader/MainPageHeader.js
@@ -0,0 +1,35 @@
+import "./MainPageHeader.scss";
+
+function MainPageNav() {
+  return (
+    <header className="main-page-header">
+      <div className="main-page-header__container">
+        <h1 className="main-page-header__title">
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+
+        <nav className="main-page-header__nav-menu">
+          <ul className="main-page-header__menu">
+            <li className="main-page-header__menu_item">
+              <a href="#" className="main-page-header__menu_link">
+                О проекте
+              </a>
+            </li>
+            <li className="main-page-header__menu_item">
+              <a href="#" className="main-page-header__menu_link">
+                Технологии
+              </a>
+            </li>
+            <li className="main-page-header__menu_item">
+              <a href="#" className="main-page-header__menu_link">
+                Студент
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default MainPageNav;
