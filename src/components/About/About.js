758ba220994c8b1d25c8c14321096c9912e8476c
Dergach Roman <dergachroma@gmail.com>
2022-07-10 15:41:32 +0300

    feat: адаптировал секцию about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,29 +4,36 @@ function About() {
   return (
     <section id="about" className="about">
       <h2 className="about__title">О проекте</h2>
-      <section className="about__fact">
-        <h3 className="about__fact_title">Дипломный проект включал 5 этапов</h3>
-        <p className="about__fact_subtitle">
-          Составление плана, работу над бэкендом, вёрстку, добавление
-          функциональности и финальные доработки.
-        </p>
-      </section>
-      <section className="about__fact">
-        <h3 className="about__fact_title">Дипломный проект включал 5 этапов</h3>
-        <p className="about__fact_subtitle">
-          Составление плана, работу над бэкендом, вёрстку, добавление
-          функциональности и финальные доработки.
-        </p>
-      </section>
+      <div className="about__facts">
+        <section className="about__fact">
+          <h3 className="about__fact_title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about__fact_subtitle">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </section>
+        <section className="about__fact">
+          <h3 className="about__fact_title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about__fact_subtitle">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </section>
+      </div>
+
       <div className="about__schedule">
-        <div className="about__schedule_el about__schedule_el_1">
+        <section className="about__schedule_el about__schedule_el_1">
           <h3 className="about__schedule_title">1 неделя</h3>
           <p className="about__schedule_subtitle">Back-end</p>
-        </div>
-        <div className="about__schedule_el about__schedule_el_2">
+        </section>
+        <section className="about__schedule_el about__schedule_el_2">
           <h3 className="about__schedule_title">4 недели</h3>
           <p className="about__schedule_subtitle">Front-end</p>
-        </div>
+        </section>
       </div>
     </section>
   );
